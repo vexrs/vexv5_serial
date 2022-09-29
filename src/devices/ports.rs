ebaca5bad131c4101ec6b560b190d5886d0a1436
Riley Wilton <riley.j.wilton@gmail.com>
2022-09-29 14:55:51 -0400

    Device discovery complete.

--- src/devices/ports.rs
+++ src/devices/ports.rs
@@ -1,11 +1,15 @@
 use serialport::SerialPortInfo;
 use anyhow::Result;
 
+const VEX_V5_BRAIN_PID: u16 = 0x0501;
+const VEX_V5_CONTROLLER_PID: u16 = 0x0503;
+const VEX_VID: u16 = 0x2888;
 
 /// This enum represents three types of Vex serial devices:
 /// The User port for communication with the user program.
 /// The System port for communicating with VexOS.
 /// And the Controller port for communicating with the VexV5 joystick
+#[derive(PartialEq, Debug)]
 pub enum VexSerialType {
     User,
     System,
@@ -13,6 +17,7 @@ pub enum VexSerialType {
 }
 
 /// This structure incapsulates the information for a vex v5 serial port.
+#[derive(Debug)]
 pub struct VexSerialInfo {
     pub port_info: SerialPortInfo,
     pub port_type: VexSerialType,
@@ -39,8 +44,53 @@ pub fn discover_vex_ports() -> Result<Vec<VexSerialInfo>> {
             _ => continue, // Skip the port if it is not USB.
         };
 
+        // If it does not have a Vex Vendor ID, then skip it
+        if port_info.vid != VEX_VID {
+            continue;
+        }
+
+        // If the Product ID is a Vex Joystick, add it
+        if port_info.pid == VEX_V5_CONTROLLER_PID {
+            vex_ports.push(VexSerialInfo {
+                port_info: port,
+                port_type: VexSerialType::Controller,
+            });
+        } else if port_info.pid == VEX_V5_BRAIN_PID {
+            // If it is a brain, add it to the list determining if it is system or user
+            vex_ports.push(VexSerialInfo {
+                port_info: port,
+                port_type: {
+                    // Get the product name
+                    let name = match port_info.product {
+                        Some(s) => s,
+                        _ => continue,
+                    };
+
+                    // If the name contains User, it is a User port
+                    if name.contains("User"){
+                        VexSerialType::System
+                    } else if name.contains("Communications") {
+                        // If the name contains Communications, is is a System port.
+                        VexSerialType::User
+                    } else if match vex_ports.last() {
+                            Some(p) => p.port_type == VexSerialType::System,
+                            _ => false,
+                        } {
+                        // PROS source code also hints that User will always be listed after System
+                        VexSerialType::User
+                    } else {
+                        // If the previous one was user or the vector is empty,
+                        // The PROS source code says that this one is most likely System.
+                        VexSerialType::System
+                    }
+                    
+                }
+            });
+
+        }
+
     }
 
     // Return the vector of discovered ports
     Ok(vex_ports)
-};
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,5 @@
 fn main() {
+    let vex_ports = vexv5_serial::devices::ports::discover_vex_ports().unwrap();
+
+    println!("{:?}", vex_ports);
 }
