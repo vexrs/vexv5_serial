0191b5381eacc8418cc9f6d5dcf78868bd27ee6d
Riley Wilton <riley.j.wilton@gmail.com>
2022-11-17 14:25:12 -0500

    Expected functionality in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     let vex_ports = vexv5_serial::devices::get_socket_info_pairs().unwrap();
 
     println!("{:?}", vex_ports);
+
+    let device = vexv5_serial::v5::Device::new(vex_ports.0);
+
+    let res = device.send_command(vexv5_serial::commands::SystemKeyValueWrite("teamnumber", b"7122b"));
+
+    println!("{:?}", res)
 }
