2544e64b1a0cd42129f6bbb49a6cc8b1cb3d98a7
Riley Wilton <riley.j.wilton@gmail.com>
2022-11-18 15:09:43 -0500

    Fixed some errors in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 
 
 fn main() -> anyhow::Result<()>{
-    let vex_ports = vexv5_serial::devices::get_socket_info_pairs().unwrap();
+    let vex_ports = vexv5_serial::devices::get_socket_info_pairs()?;
 
-    let ports = vexv5_serial::devices::open_device(vex_ports[0].clone())?;
+    let ports = vexv5_serial::devices::open_device(&vex_ports[0])?;
 
     let mut device = vexv5_serial::v5::Device::new(ports.0, ports.1);
 
-    let res = device.send_request(vexv5_serial::commands::KVWrite("teamnumber", "7122A"));
+    let _ = device.send_request(vexv5_serial::commands::KVWrite("teamnumber", "7122A"))?;
 
-    let res = device.send_request(vexv5_serial::commands::KVRead("teamnumber")).unwrap();
+    let res = device.send_request(vexv5_serial::commands::KVRead("teamnumber"))?;
 
     println!("{:?}", res);
 
