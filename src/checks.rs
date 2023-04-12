3b7069481c92414d8c18cc3ec56a56c6929303d1
Riley Wilton <riley.j.wilton@gmail.com>
2023-04-12 13:32:58 -0400

    Improved documentation in checks.rs

--- src/checks.rs
+++ src/checks.rs
@@ -5,10 +5,15 @@ bitflags! {
     /// These flags determine what checks recieve_extended will perform
     /// on the recieved packet.
     pub struct VexExtPacketChecks: u8 {
+        /// Do not perform any checks
         const NONE = 0b00000000;
+        /// Bit 1 requires that we check the ACK value
         const ACK = 0b00000001;
+        /// Bit 2 requires that we check the CRC
         const CRC = 0b00000010;
+        /// Bit 3 requires that we check the Length of the packet
         const LENGTH = 0b00000100;
+        /// This member sets all of the flags (except for none, which is an absence of flags)
         const ALL = Self::ACK.bits | Self::CRC.bits | Self::LENGTH.bits;
     }
 }
